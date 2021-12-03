23a20729262e600fb5866a78ba1526ea86163ef7
Aditi Krishna <krishna.ad@northeastern.edu>
2021-12-03 18:06:02 -0500

    Pet Therapy admin backend

--- src/Business/PetTherapy/PetTherapyStaff.java
+++ src/Business/PetTherapy/PetTherapyStaff.java
@@ -14,14 +14,94 @@ public class PetTherapyStaff {
     int id;
     int count =1;
     
-    public PetTherapyStaff(){
-        
-        id = count;
-        count++;
+    String Id;
+    String petname;
+    String employeename;
+    String email;
+    String phone;
+    String address;
+    String city;
+    String country;
+    String zipcode;
+
+    public int getCount() {
+        return count;
     }
 
-    public int getId() {
-        return id;
+    public void setCount(int count) {
+        this.count = count;
     }
-    
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getPetname() {
+        return petname;
+    }
+
+    public void setPetname(String petname) {
+        this.petname = petname;
+    }
+
+    public String getEmployeename() {
+        return employeename;
+    }
+
+    public void setEmployeename(String employeename) {
+        this.employeename = employeename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+        
 }
